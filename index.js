3bfb3654f1cb1056b0a2891f5cd036007e3d8a4e
Qca-2023 <162576173+Qca-2023@users.noreply.github.com>
2024-10-03 11:01:13 +0530

    Removed the frontend part from index.js

--- index.js
+++ index.js
@@ -43,17 +43,4 @@ mongoose
   });
 
 
-// src/index.js
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
 
